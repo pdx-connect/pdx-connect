271da706a3f976b67bf74d0156f7d78a35fe8d92
Bradley Odell <btodell@hotmail.com>
2019-03-07 20:41:57 -0800

    Removed unnecessary typings for react-bootstrap (that was causing the build to break). Implemented basic Express app for server on port 9999.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,13 @@
-import express = require("express");
-import {Express} from "express";
+import * as express from "express";
+import {Express, Request, Response} from "express";
 
+const port: number = 9999;
 const app: Express = express();
 
+app.get("/", (request: Request, response: Response) => {
+    response.send("Hello, world!");
+});
+
+app.listen(port, () => {
+    console.log("Server has started on port: " + port);
+});
