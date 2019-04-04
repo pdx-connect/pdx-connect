fff9f080df1a0a7ba6a82c6be7e21c24289e3e2d
Brooke Dedrickson <mdedri2@pdx.edu>
2019-04-04 02:37:28 -0700

    Added css changes, removed unnecessary import

--- client/src/pages/SearchResults.tsx
+++ client/src/pages/SearchResults.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {Component, ReactNode} from "react";
-import { Container, Row, Col, Form, Button, Modal} from "react-bootstrap"
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
+import "./SearchResults.css"
 
 interface Props {
     searchField?: string;
@@ -29,16 +29,16 @@ export class SearchResults extends Component<Props, State> {
      */
     public render(): ReactNode {
         const data = [
-            {index: '1', picture: 'Img1', name: 'John Doe', major: 'Computer Science', tags: 'Art boy'},
+            {index: '1', picture: 'Img1', name: 'John Doe', major: 'Computer Science', tags: 'Art boy, Portland, On-campus'},
             {index: '2', picture: 'Img2', name: 'Random Person', major: 'Art', tags: 'Portland'}
         ]
         return (
             <BootstrapTable data={ data } options={ { noDataText: 'There is no search results' }}>
-                <TableHeaderColumn dataField='index' isKey>#</TableHeaderColumn>
-                <TableHeaderColumn dataField='picture'>Profile Picture</TableHeaderColumn>
-                <TableHeaderColumn dataField='name'>Name</TableHeaderColumn>
-                <TableHeaderColumn dataField='major'>Major</TableHeaderColumn>
-                <TableHeaderColumn dataField='tags'>Tags</TableHeaderColumn>
+                <TableHeaderColumn dataField='index' isKey={true} hidden={true} className={'header'}>#</TableHeaderColumn>
+                <TableHeaderColumn dataField='picture' className={'header'}>Profile Picture</TableHeaderColumn>
+                <TableHeaderColumn dataField='name' className={'header'}>Name</TableHeaderColumn>
+                <TableHeaderColumn dataField='major' className={'header'}>Major</TableHeaderColumn>
+                <TableHeaderColumn dataField='tags' className={'header'}>Tags</TableHeaderColumn>
             </BootstrapTable>
         );
     }
