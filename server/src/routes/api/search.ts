3c8c8cc4f2b4c30811b9842afb9a1a892d44495f
Yian Chen <y22@pdx.edu>
2019-04-05 00:19:25 -0700

    Search by display name and by major for profiles

---
+++ server/src/routes/api/search.ts
@@ -0,0 +1,86 @@
+import {Express, Request, Response} from "express";
+import {Connection, Like} from "typeorm";
+import {User} from "../../entity/User";
+import {UserProfile} from "../../entity/UserProfile";
+import {Tag} from "../../entity/Tag";
+
+
+export function route(app: Express, db: Connection) {
+    app.post("/api/search/profile", async (request: Request, response: Response) => {
+        var USERS: Object[]|undefined = undefined;
+
+        if(request.body.searchBy === 1)     // Search by display name
+        {
+            if (request.body.displayName != null) {
+                // Search the DB to find all users with this displayName
+                const users: User[] = await User.find({
+                    where: {
+                        displayName: Like("%" + request.body.displayName + "%")
+                    }
+                });
+
+                // Create an array of user(s) containing their ID, displayName, major
+                USERS = await Promise.all(users.map(async user => {
+                    const user_profile: UserProfile|undefined = await user.profile;
+
+                    if(user_profile != undefined){
+                        return {
+                            userID: user.id,
+                            displayName: user.displayName,
+                            major: await user_profile.major
+                        };
+                    }
+                    else {
+                        return {
+                            userID: user.id,
+                            displayName: user.displayName
+                        };
+                    }
+                }))
+            } else {
+                response.sendStatus(400);
+            }
+
+        }
+        else if(request.body.searchBy === 2)    // Search by major
+        {
+            if (request.body.major != null) {
+                // Found the tag id of the coressponding tag
+                const Major: Tag|undefined = await Tag.findOne({
+                    where: {
+                        name: request.body.major
+                    }
+                });
+    
+                if(Major != undefined)
+                {
+                    // Search the DB to find all users with this major
+                    const user_profiles: UserProfile[] = await UserProfile.find({
+                        where: {
+                            major: Major.id
+                        }
+                    });
+
+                    // Create an array of user(s) containing their ID, displayName, major
+                    USERS = await Promise.all(user_profiles.map(async user_profile => {
+                        const user: User = await user_profile.user;
+
+                        return {
+                            userID: user_profile.userID,
+                            displayName: user.displayName,
+                            major: await user_profile.major
+                        };
+                    }))
+                }
+            } else {
+                response.sendStatus(400);
+            }
+        }
+
+        response.send(JSON.stringify({
+            // Send back the array of found user(s)
+            users: USERS
+        }));
+
+    });
+}
