2e125fc777ee27bd6928c093bd375b0cfb8220db
Bradley Odell <btodell@hotmail.com>
2019-04-04 22:08:24 -0700

    Added 'express-ws' package and configured an example route at the root.

---
+++ server/src/routes/ws.ts
@@ -0,0 +1,11 @@
+import {Express, Request, NextFunction} from "express";
+import {Connection} from "typeorm";
+import * as expressWs from "express-ws";
+import * as ws from "ws";
+
+export function route(app: Express, db: Connection) {
+    const appWS: expressWs.Application = expressWs(app).app;
+    appWS.ws("/", (socket: ws, req: Request, next: NextFunction) => {
+        // TODO Handle websocket connection
+    });
+}
