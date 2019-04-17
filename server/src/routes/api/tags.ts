89c1f621c401d6852403b7c4bf8c71d7bea6ab8c
Bradley Odell <btodell@hotmail.com>
2019-04-16 20:47:57 -0700

    Add Tag helper functions
    Implemented helper functions in Tag entity class for getting descendant tags.
    Updated tag routes to return major tags.
    Cleaned up some of the search routes.

--- server/src/routes/api/tags.ts
+++ server/src/routes/api/tags.ts
@@ -2,18 +2,39 @@ import {Express, Request, Response} from "express";
 import {Connection} from "typeorm";
 import {Tag} from "../../entity/Tag";
 
+interface TagData {
+    id: number;
+    name: string;
+}
+
 export function route(app: Express, db: Connection) {
     app.get("/api/tags", async (request: Request, response: Response) => {
-        let json: {
-            id: number;
-            name: string;
-        }[] | string;
+        let json: TagData[] | string;
         if (request.isAuthenticated()) {
             const tags: Tag[] =  await Tag.find();
             json = [];
             for (const tag of tags) {
                 json.push({
                     id: tag.id,
+                    name: tag.name
+                });
+            }
+        } else {
+            json = "Not logged in.";
+        }
+        response.send(JSON.stringify(json));
+    });
+    app.get("/api/tags/majors", async (request: Request, response: Response) => {
+        let json: TagData[] | string;
+        if (request.isAuthenticated()) {
+            json = [];
+            // Get the major tag
+            const majorTag: Tag = await Tag.findMajor();
+            // Get all of its leaf nodes, which are all the academia majors
+            const majors: Map<number, Tag> = await majorTag.getLeafDescendents();
+            for (const [id, tag] of majors) {
+                json.push({
+                    id: id,
                     name: tag.name
                 });
             }
