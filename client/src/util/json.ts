aa3da0b67afe1eb019e905827e74e5279aab6660
Bradley Odell <btodell@hotmail.com>
2019-04-16 21:55:30 -0700

    Created utility functions.

---
+++ client/src/util/json.ts
@@ -0,0 +1,24 @@
+/**
+ * Sends a GET request to the given route and returns the JSON response.
+ */
+export function getJSON(route: string): Promise<unknown> {
+    return fetch(route, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(response => response.json());
+}
+
+/**
+ * Sends a POST request to the given route with the given JSON-serializable data.
+ */
+export function postJSON(route: string, data: any): Promise<unknown> {
+    return fetch(route, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }).then(response => response.json());
+}
