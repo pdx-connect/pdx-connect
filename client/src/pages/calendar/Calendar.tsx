604030457ca8cf0370651e1b72f445ffb992e7a4
doanh <doanh@pdx.edu>
2019-04-18 13:36:14 -0700

    added calendar set up - missing style

--- client/src/pages/calendar/Calendar.tsx
+++ client/src/pages/calendar/Calendar.tsx
@@ -1,34 +1,115 @@
 import * as React from "react";
-import {Component, ReactNode} from "react";
+import { Component, ReactNode } from "react";
+import BigCalendar, { BigCalendarProps, Navigate, View, DateRange, DateLocalizer, ToolbarProps, EventProps, EventWrapperProps } from "react-big-calendar";
+import moment from "moment";
 
+const localizer = BigCalendar.momentLocalizer(moment);
 
-interface Props {
-}
+interface Props {}
 
 interface State {
+  events: any;
+  style: any;
+  height: any;
 }
+var today = new Date();
+var y = today.getFullYear();
+var m = today.getMonth();
+var d = today.getDate();
+
+declare const allViews: View[];
+
+const events = [
+  {
+    title: "All Day Event",
+    allDay: true,
+    start: new Date(y, m, 1),
+    end: new Date(y, m, 1),
+    color: "default"
+  },
+  {
+    title: "Meeting",
+    start: new Date(y, m, d - 1, 10, 30),
+    end: new Date(y, m, d - 1, 11, 30),
+    allDay: false,
+    color: "green"
+  },
+  {
+    title: "Lunch",
+    start: new Date(y, m, d + 7, 12, 0),
+    end: new Date(y, m, d + 7, 14, 0),
+    allDay: false,
+    color: "red"
+  },
+  {
+    title: "Nud-pro Launch",
+    start: new Date(y, m, d - 2),
+    end: new Date(y, m, d - 2),
+    allDay: true,
+    color: "azure"
+  },
+  {
+    title: "Birthday Party",
+    start: new Date(y, m, d + 1, 19, 0),
+    end: new Date(y, m, d + 1, 22, 30),
+    allDay: false,
+    color: "azure"
+  },
+  {
+    title: "Team C meeting",
+    start: new Date(y, m, 21),
+    end: new Date(y, m, 22),
+    color: "orange"
+  },
+  {
+    title: "Click for Google",
+    start: new Date(y, m, 21),
+    end: new Date(y, m, 22),
+    color: "orange"
+  }
+];
 
 /**
- * 
+ *
  */
+
+const style = {
+  "min-height": 500
+};
+
 export class Calendar extends Component<Props, State> {
-    
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-        };
-    }
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      events: events,
+      style: "",
+      height: "100vh"
+    };
+  }
 
-  
-    
-    /**
-     * @override
-     */
-    public render(): ReactNode {
-
-        return (
-                <pre>This is the calendar page</pre>
-        );
-    }
-
+  /**
+   * @override
+   */
+  public render(): ReactNode {
+    return (
+      <div style={{ height: 700 }}>
+      <BigCalendar
+          events={this.state.events}
+          views={allViews}
+          step={60}
+          showMultiDayTimes
+          defaultDate={new Date(2015, 3, 1)}
+          localizer={localizer}
+      />
+        {/* <BigCalendar
+          selectable
+          localizer={localizer}
+          events={this.state.events}
+          startAccessor="start"
+          endAccessor="end"
+        //   style={{ height: "100vh" }}
+        /> */}
+      </div>
+    );
+  }
 }
