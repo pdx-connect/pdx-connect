337c2961f98638b3374b0ce806213af537bf13a8
David <idavidgeo@gmail.com>
2019-04-25 17:45:07 -0700

    Fixed textbox

--- client/src/pages/inbox/Inbox.tsx
+++ client/src/pages/inbox/Inbox.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import {Container, Row, Col, Form, FormControl, Button} from "react-bootstrap";
 import {Component, ReactNode} from "react";
-
 import {Message, ConversationEntry} from "../Home";
-
 import "./Inbox.css";
 
 
@@ -19,150 +17,6 @@ interface State {
     currentConversation: number;
     textField: string;
 }
-
-const conversations = [
-    {   
-        conversationID: 100,
-        lastSeen: 1555011969, // epoch
-        messages: [
-            {
-                userID: 6,
-                timeSent: 1555011169,
-                text: "I am David!",
-                seen: false
-            },
-            {
-                userID: 4,
-                timeSent: 1555011175,
-                text: "I am Daniel!",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011179,
-                text: "Whats up?",
-                seen: false
-            },
-            {
-                userID: 4,
-                timeSent: 1555011186,
-                text: "Nothing much",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011197,
-                text: "Are you actually Daniel?",
-                seen: false
-            },
-            {
-                userID: 4,
-                timeSent: 1555011201,
-                text: "Nah I'm David lol",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011210,
-                text: "Ah, you got me for a sec",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011220,
-                text: "Ayyyyyyyyyyyyyyyy",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011225,
-                text: "Ayyyyyy",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011225,
-                text: "Ayyyyyy",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011225,
-                text: "Ayyyyyy",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011225,
-                text: "Ayyyyyy",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011225,
-                text: "Ayyyyyy",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011225,
-                text: "Ayyyyyy",
-                seen: false
-            },            {
-                userID: 6,
-                timeSent: 1555011225,
-                text: "Ayyyyyy",
-                seen: false
-            },            {
-                userID: 6,
-                timeSent: 1555011225,
-                text: "Ayyyyyy",
-                seen: false
-            },            {
-                userID: 6,
-                timeSent: 1555011225,
-                text: "Ayyyyyy",
-                seen: false
-            }
-        ]
-    },
-    {   
-        conversationID: 101,
-        lastSeen: 1555011959, // epoch
-        messages: [
-            {
-                userID: 4,
-                timeSent: 1555011169,
-                text: "I am user 1",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011175,
-                text: "I am user 2",
-                seen: false
-            }
-        ]
-    },
-    {   
-        conversationID: 102,
-        lastSeen: 1555011969, // epoch
-        messages: [
-            {
-                userID: 4,
-                timeSent: 1555011169,
-                text: "Hello world",
-                seen: false
-            },
-            {
-                userID: 6,
-                timeSent: 1555011175,
-                text: "ayee",
-                seen: false
-            }
-        ]
-    }
-];
 
 /**
  * 
@@ -184,7 +38,8 @@ export class Inbox extends Component<Props, State> {
     private readonly onSubmit = (e: any) => {
         e.preventDefault();
 
-        this.props.sendMessage(this.state.textField, this.state.currentConversation, null);        
+        this.props.sendMessage(this.state.textField, this.state.currentConversation, null);
+        console.log("Sending message!", this.state.textField, this.state.currentConversation);       
 
         console.log(this.state.textField);
         this.setState({textField: ""});
@@ -199,8 +54,8 @@ export class Inbox extends Component<Props, State> {
                     rows.push(
                         <Row key={i} onClick={()=> this.setState({currentConversation: i})} className="open-conversation">
                             <Col key={i} sm={12}>
-                                Message from: user {this.props.conversations[i].entries[0].userID}
-                                Preview: {this.props.conversations[i].entries[this.props.conversations[i].entries.length-1].text}
+                                Message from: user {this.props.conversations[i].entries[0].userID} {/* Gets the latest message sender */}
+                                Preview: {this.props.conversations[i].entries[0].text} {/* Gets the latest message as preview */}
                             </Col>
                         </Row>
                     );
@@ -210,13 +65,13 @@ export class Inbox extends Component<Props, State> {
                         <Row key={i} onClick={()=> this.setState({currentConversation: i})} className="conversation">
                             <Col key={i} sm={12}>
                                 Message from: user {this.props.conversations[i].entries[0].userID}
-                                Preview: {this.props.conversations[i].entries[this.props.conversations[i].entries.length-1].text}
+                                Preview: {this.props.conversations[i].entries[0].text}
                             </Col>
                         </Row>
                     );
                 }
             }
-        } else {
+        } else{
             rows.push(
             <Row key={0} className="no-message">
                 <Col key={0} sm={12}>No conversations</Col>
@@ -228,8 +83,9 @@ export class Inbox extends Component<Props, State> {
     private readonly getMessages = () => {
         let rows = [];
         if (this.props.conversations != null) {
-            for (let i=0; i<this.props.conversations[this.state.currentConversation].entries.length; i++) {
-                if (this.props.conversations[0].entries[i].userID == this.props.userID) {
+            console.log("Convo 0" ,this.props.conversations[0]) 
+            for (let i=0; i<this.props.conversations[this.state.currentConversation].entries.length; i++) { 
+                if (this.props.conversations[this.state.currentConversation].entries[i].userID == this.props.userID) {
                     rows.push(
                         <Row key={i} className="my-message">
                             <Col key={i} sm={12}> {this.props.conversations[this.state.currentConversation].entries[i].text}</Col>
@@ -256,13 +112,13 @@ export class Inbox extends Component<Props, State> {
     /**
      * @override
      */
-    public shouldComponentUpdate(nextProps: Props, nextState: State) {
-        if (nextProps != this.props) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // public shouldComponentUpdate(nextProps: Props, nextState: State) {
+    //     if (nextProps != this.props) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
     /**
      * @override
@@ -302,7 +158,7 @@ export class Inbox extends Component<Props, State> {
                 <div className="text-box">
                     <Form onSubmit={(e: any) => this.onSubmit(e)}>
                         <Row>
-                            <Col><Form.Control onChange={(e: any) => this.onChange(e)} type="text" value={this.state.textField} placeholder="Enter message..."/></Col>
+                            <Col><Form.Control className="textField" onChange={(e: any) => this.onChange(e)} type="text" value={this.state.textField} placeholder="Enter message..."/></Col>
                             <Col><Button variant="primary" type="submit">Send</Button></Col>
                         </Row>
                     </Form>
