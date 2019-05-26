d5377dcf4aad75557ae901aa70b85298553490cb
Bradley Odell <btodell@hotmail.com>
2019-05-25 22:40:32 -0700

    Added 'type' column to the Tag entity and update tag routes.

--- server/src/routes/api/tags.ts
+++ server/src/routes/api/tags.ts
@@ -1,6 +1,6 @@
 import {Express, Request, Response} from "express";
 import {Connection} from "typeorm";
-import {Tag} from "../../entity/Tag";
+import {Tag, TagType} from "../../entity/Tag";
 
 interface TagData {
     id: number;
@@ -10,20 +10,19 @@ interface TagData {
 interface Node {
     name: string;
     children: Node[];
-};
+}
 
 export function route(app: Express, db: Connection) {
     app.get("/api/tags", async (request: Request, response: Response) => {
         let json: TagData[] | string;
         if (request.isAuthenticated()) {
             const tags: Tag[] =  await Tag.find();
-            json = [];
-            for (const tag of tags) {
-                json.push({
+            json = tags.map(tag => {
+                return {
                     id: tag.id,
                     name: tag.name
-                });
-            }
+                };
+            });
         } else {
             json = "Not logged in.";
         }
@@ -32,17 +31,17 @@ export function route(app: Express, db: Connection) {
     app.get("/api/tags/majors", async (request: Request, response: Response) => {
         let json: TagData[] | string;
         if (request.isAuthenticated()) {
-            json = [];
-            // Get the major tag
-            const majorTag: Tag = await Tag.findMajor();
-            // Get all of its leaf nodes, which are all the academia majors
-            const majors: Map<number, Tag> = await majorTag.getLeafDescendents();
-            for (const [id, tag] of majors) {
-                json.push({
-                    id: id,
-                    name: tag.name
-                });
-            }
+            const majorTags: Tag[] = await Tag.find({
+                where: {
+                    type: TagType.MAJOR
+                }
+            });
+            json = majorTags.map(majorTag => {
+                return {
+                    id: majorTag.id,
+                    name: majorTag.name
+                };
+            });
         } else {
             json = "Not logged in.";
         }
@@ -50,32 +49,31 @@ export function route(app: Express, db: Connection) {
     });
     app.get("/api/tags/allBaseTags", async (request: Request, response: Response) => {
         let json: TagData[] | string;
-        let parents: Tag[];
         if (request.isAuthenticated()) {
             json = [];
-            parents = [];
-
-            const tags: Tag[] =  await Tag.find();
-            // Gets all the most top level parent tags' name
-            for (const tag of tags) {
-                if(Object.keys(await tag.parents).length == 0)
-                {
-                    parents.push(tag);
-                }
-            }
-
-            // Loop through to find all their leaf nodes
-            for(const parent of parents)
-            {
-                const children: Map<number, Tag> = await parent.getLeafDescendents();
-                for (const [id, tag] of children)
-                {
-                    json.push({
-                        id: id,
-                        name: tag.name
-                    });                
-                }
-            }
+            // let parents: Tag[] = [];
+            //
+            // const tags: Tag[] =  await Tag.find();
+            // // Gets all the most top level parent tags' name
+            // for (const tag of tags) {
+            //     if(Object.keys(await tag.parents).length == 0)
+            //     {
+            //         parents.push(tag);
+            //     }
+            // }
+            //
+            // // Loop through to find all their leaf nodes
+            // for(const parent of parents)
+            // {
+            //     const children: Map<number, Tag> = await parent.getLeafDescendents();
+            //     for (const [id, tag] of children)
+            //     {
+            //         json.push({
+            //             id: id,
+            //             name: tag.name
+            //         });                
+            //     }
+            // }
         } else {
             json = "Not logged in.";
         }
@@ -85,63 +83,59 @@ export function route(app: Express, db: Connection) {
 
     // The most top level parent tags
     app.get("/api/tags/tagTree", async (request: Request, response: Response) => {
-        let json: TagData[] | string;
-
-        let TagTree: Node[];
+        let tagTree: Node[];
         if (request.isAuthenticated()) {
-            json = [];
-            TagTree = [];
-            const tags: Tag[] =  await Tag.find();
-            // Gets all the most top level parent tags' name
-            for (const tag of tags) {
-                if(Object.keys(await tag.parents).length == 0)
-                {
-                    TagTree.push({
-                        name: tag.name,
-                        children: []
-                    });
-                }
-            }
-            // Build a tree structure of the entire tag relation db
-            TagTree = await treeTraversal(TagTree);
-
+            // const tags: Tag[] =  await Tag.find();
+            // const topTags: Node[] = [];
+            // // Gets all the most top level parent tags' name
+            // for (const tag of tags) {
+            //     if(Object.keys(await tag.parents).length == 0)
+            //     {
+            //         topTags.push({
+            //             name: tag.name,
+            //             children: []
+            //         });
+            //     }
+            // }
+            // // Build a tree structure of the entire tag relation db
+            // tagTree = await treeTraversal(topTags);
+            tagTree = [];
         } else {
-            json = "Not logged in.";
-            TagTree = [];
+            tagTree = [];
         }
-        response.send(JSON.stringify(TagTree));
+        response.send(JSON.stringify(tagTree));
     });
 
-    // Recursive function
-    async function treeTraversal(parent: Node[]): Promise<Node[]> {
-        // Add children
-        for(const subtree of parent) {
-            subtree.children = await findChildren(subtree.name);
-        }
-
-        for(const subtree of parent) {
-            subtree.children = await treeTraversal(subtree.children);
-        }
-        return parent;
-    }
-
-    // Helper function
-    async function findChildren(name: string):Promise<Node[]> {
-        let subtrees: Node[] = [];
-        const current: Tag|undefined = await Tag.findOne({
-            name: name
-        })
-        if(current)
-        {
-            for (const child of await current.children)
-            {
-                const temp: Node = {
-                    name: child.name,
-                    children: []
-                }
-                subtrees.push(temp);
-            }
-        }
-        return subtrees;
-    }
+    // // Recursive function
+    // async function treeTraversal(parent: Node[]): Promise<Node[]> {
+    //     // Add children
+    //     for(const subtree of parent) {
+    //         subtree.children = await findChildren(subtree.name);
+    //     }
+    //
+    //     for(const subtree of parent) {
+    //         subtree.children = await treeTraversal(subtree.children);
+    //     }
+    //     return parent;
+    // }
+    //
+    // // Helper function
+    // async function findChildren(name: string):Promise<Node[]> {
+    //     let subtrees: Node[] = [];
+    //     const current: Tag|undefined = await Tag.findOne({
+    //         name: name
+    //     })
+    //     if(current)
+    //     {
+    //         for (const child of await current.children)
+    //         {
+    //             const temp: Node = {
+    //                 name: child.name,
+    //                 children: []
+    //             }
+    //             subtrees.push(temp);
+    //         }
+    //     }
+    //     return subtrees;
+    // }
 }
